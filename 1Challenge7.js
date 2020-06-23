e0100237e0e862f65294534fff3322f3b174d11e
Bianca Amorim <bia.rrii.eco@gmail.com>
2020-06-23 16:02:13 -0300

    update Challenge7

---
+++ 1Challenge7.js
@@ -0,0 +1,82 @@
+// Challenge 1-7: First steps with JS - to strengthen some concepts, among them:
+// Booleans, Organization, Standardization, Writing.
+
+// Create a programS like exemṕles in:
+//https://github.com/Rocketseat/bootcamp-launchbase-desafios-01/blob/master/desafios/01-4-aplicacao-operacoes-bancarias.md
+
+
+// Create a program to perform banking transactions on a user's account.
+
+// Banking operations
+
+const user = {
+    name: 'Mariana',
+    transactions: [],
+    balance: 0
+}
+
+// Add transactions
+function createTransaction(transaction) {
+    user.transactions.push(transaction)
+
+    if (transaction.type === 'credit') {
+        user.balance = user.balance + transaction.value
+    } else {
+        user.balance = user.balance - transaction.value
+    }
+}
+
+// Reporting
+function getHigherTransactionByType(type) {
+    let higherTransaction
+    let higherValue = 0
+
+    for (let transaction of user.transactions) {
+        if (transaction.type == type && transaction.value > higherValue) {
+            higherValue = transaction.value
+            higherTransaction = transaction
+        }
+    }
+
+    return higherTransaction
+}
+
+function getAverageTransactionValue() {
+    let sum = 0
+
+    for(let transaction of user.transactions) {
+        sum += transaction.value
+    }
+
+    return sum / user.transactions.length
+}
+
+function getTransactionsCount() {
+    let count = {
+        credit: 0,
+        debit: 0,
+    }
+    for (let transaction of user.transactions) {
+        if (transaction.type === 'credit')
+            count.credit++
+        else
+            count.debit++
+    }
+
+    return count
+}
+
+
+createTransaction({ type: 'credit', value: 50 })
+createTransaction({ type: 'credit', value: 120 })
+createTransaction({ type: 'debit', value: 80 })
+createTransaction({ type: 'debit', value: 30 })
+
+console.log(user.balance) // 60
+
+console.log(getHigherTransactionByType('credit')) // { type: 'credit', value: 120 }
+console.log(getHigherTransactionByType('debit')) // { type: 'debit', value: 80 }
+
+console.log(getAverageTransactionValue()) // 70
+
+console.log(getTransactionsCount()) // { credit: 2, debit: 2 }
